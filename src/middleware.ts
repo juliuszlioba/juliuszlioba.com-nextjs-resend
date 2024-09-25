b1c38a0e83293affe8a4b6221cc2f90c73456e63
Julius Zlioba <julius.zlioba@connect724.at>
2024-09-25 13:29:47 +0200

    updated middleware to PROD

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,26 +10,26 @@ export function middleware(request: NextRequest) {
 	const origin = request.headers.get('origin')
 
   // DEV
-	if (origin && !allowedOrigins.includes(origin)) {
-    return new NextResponse(null, {
-      status: 400,
-      statusText: "Bad Request",
-      headers: {
-        "Content-Type": "text/plain",
-      },
-    });
-  }
+	// if (origin && !allowedOrigins.includes(origin)) {
+  //   return new NextResponse(null, {
+  //     status: 400,
+  //     statusText: "Bad Request",
+  //     headers: {
+  //       "Content-Type": "text/plain",
+  //     },
+  //   });
+  // }
 
 	// PRODUCTION
-	// if ((origin && !allowedOrigins.includes(origin)) || !origin) {
-	// 	return new NextResponse(null, {
-	// 		status: 400,
-	// 		statusText: 'Bad Request',
-	// 		headers: {
-	// 			'Content-Type': 'text/plain',
-	// 		},
-	// 	})
-	// }
+	if ((origin && !allowedOrigins.includes(origin)) || !origin) {
+		return new NextResponse(null, {
+			status: 400,
+			statusText: 'Bad Request',
+			headers: {
+				'Content-Type': 'text/plain',
+			},
+		})
+	}
 
 	return NextResponse.next()
 }
